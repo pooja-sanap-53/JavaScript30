c691ead8c570692dac5a6539ba92e9c9df4f792c
Pooja Sanap <sanappooja777@gmail.com>
2022-08-20 01:53:29 +0530

    Completed day 1
    learnt about javascript variables

---
+++ DAY 01/intro_variables.js
@@ -0,0 +1,23 @@
+// Day 01
+
+console.log("Hello JavaScript!"); 
+
+//Types of Variables
+// Integer
+var myInteger = 12;
+// Long
+var myLong = 9310141419482; 
+// Float
+var myFloat = 5.5;
+// Double
+var myDouble = 9310141419482.22; 
+// Boolean
+var myBoolean = true; 
+var myBoolean2 = false;
+// Not A Number 
+var myNotANumber = NaN;
+var NaN_Example = 0/0; 
+// undefined
+var notDefined; 
+// null
+var myNull = null; 
